3e576ab80805d38834db8aa47ca91fb646aef4d3
Pavel Savushkin <pavel.savushkin.mix@gmail.com>
2018-08-06 22:58:47 +0300

    Add Skypeforlinux installation script

---
+++ packages/skypeforlinux/repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+install curl apt-transport-https
+
+curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
+
+echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
