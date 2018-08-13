1fae9678100df6614bb627eb5ce89c4c9cdf332b
Pavel Savushkin <pavel.savushkin.mix@gmail.com>
2018-08-13 19:54:02 +0300

    Fix google chrome stable installer

--- packages/google-chrome-stable/repo.sh
+++ packages/google-chrome-stable/repo.sh
@@ -3,4 +3,4 @@
 install dpkg
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb [arch=$ARCH] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+echo "deb [arch=$ARCH] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
