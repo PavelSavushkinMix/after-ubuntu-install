97284313bb29e084e6e77a487a1eb861913ef147
Pavel Savushkin <pavel.savushkin.mix@gmail.com>
2018-08-14 12:02:04 +0300

    Fix postman installation

--- packages/postman/install.sh
+++ packages/postman/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 wget https://dl.pstmn.io/download/latest/linux64 -O "${DOWNLOADS_FOLDER}/postman.tar.gz"
-sudo tar -xzf postman.tar.gz -C "${PROGRAMS_FOLDER}"
-rm postman.tar.gz
+sudo tar -xzf "${DOWNLOADS_FOLDER}/postman.tar.gz" -C "${PROGRAMS_FOLDER}"
+rm "${DOWNLOADS_FOLDER}/postman.tar.gz"
