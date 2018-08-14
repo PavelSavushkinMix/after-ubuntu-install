a6d55d5842e10679e9f3654b23dd0b5bb0f38dc8
Pavel Savushkin <pavel.savushkin.mix@gmail.com>
2018-08-14 12:12:51 +0300

    Add creating of programs folder and remove variables

--- packages/phpstorm/install.sh
+++ packages/phpstorm/install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-source "${VARIABLES_FOLDER}/phpstorm.sh"
+# Folder for installation PHPStorm
+export INSTALL_PATH="$PROGRAMS_FOLDER/PHPStorm"
+
+# Path to executable file of PHPStorm
+export EXECUTABLE_PATH="$INSTALL_PATH/bin/phpstorm.sh"
+
+# URL of actual version of PHPStorm
+export PHPSTORM_URL=$(wget -O - "https://data.services.jetbrains.com/products/releases?code=PS&latest=true&type=release" 2> /dev/null | jq '.PS[] | .downloads | .linux | .link' | tr -d '"')
+
+export TEMP_FOLDER_PHPSTORM="$HOME/.tmp_phpstorm"
+
 
 # Installing the application
 rm -Rf $INSTALL_PATH
